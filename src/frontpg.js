08ddb235de8b1780c5187952d182f34553201cef
Sonakshmi Bhattacharya <sonakshmibhattacharya@gmail.com>
2025-04-12 18:08:04 +0530

    Initial

---
+++ src/frontpg.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./common.css";
+
+export default function Frontpg() {
+  return (
+        <div className="main">
+            <div className="quote">
+                <h1>Stay on top of your job applications — all in one place</h1>
+                <h3>Track, manage, and automate your job search with ease.</h3>
+            </div>
+            <div className="inup">
+                <button className="inn">Sign In</button>
+                <button className="upp">Sign Up</button>
+
+            </div>
+        </div>
+  );
+}
